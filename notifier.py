2bb1e19daf914e8950ae71178fdcb34eafdbe1bf
testsnapapprouvee <140972063+testsnapapprouvee@users.noreply.github.com>
2026-01-13 22:32:07 +0100

    Delete notifier.py

--- notifier.py
+++
@@ -1,5 +0,0 @@
-import requests, os
-
-def send(msg):
-    url = f"https://api.callmebot.com/whatsapp.php?phone={os.getenv('PHONE')}&apikey={os.getenv('WHATSAPP_KEY')}&text={msg}"
-    requests.get(url)
