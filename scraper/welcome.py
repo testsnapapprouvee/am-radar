3b8f468a55f0c8ddc1875a06dfdd3a9eadc1156f
testsnapapprouvee <140972063+testsnapapprouvee@users.noreply.github.com>
2026-01-13 21:54:05 +0100

    Create welcome.py

---
+++ scraper/welcome.py
@@ -0,0 +1,27 @@
+[
+  {
+    "name": "Edmond de Rothschild",
+    "type": "html",
+    "url": "https://www.edmond-de-rothschild.com/en/careers"
+  },
+  {
+    "name": "Franklin Templeton",
+    "type": "greenhouse",
+    "url": "https://boards.greenhouse.io/franklintempleton"
+  },
+  {
+    "name": "AQR Capital",
+    "type": "html",
+    "url": "https://www.aqr.com/about/careers"
+  },
+  {
+    "name": "Candriam",
+    "type": "welcome",
+    "slug": "candriam"
+  },
+  {
+    "name": "BNY Mellon",
+    "type": "smartrecruiters",
+    "slug": "bnymellon"
+  }
+]
